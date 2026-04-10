19cd4b46a30300e8fcf01b77200d60e898a001f4
Matteo Comisso <matteo.comisso@me.com>
2026-04-10 14:15:51 +0100

    Initial Commit

---
+++ ShakeToEjectTests/ShakeToEjectTests.swift
@@ -0,0 +1,19 @@
+//
+//  ShakeToEjectTests.swift
+//  ShakeToEjectTests
+//
+//  Created by Matteo Comisso on 10/04/2026.
+//
+
+import Testing
+@testable import ShakeToEject
+
+struct ShakeToEjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
